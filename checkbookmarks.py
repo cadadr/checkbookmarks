d0dc3267dd18f2d955f92636ec701bb07756e6c1
İ. Göktuğ Kayaalp <self@gkayaalp.com>
2017-12-10 14:59:45 +0300

    Add a command line interface.

--- checkbookmarks.py
+++ checkbookmarks.py
@@ -15,24 +15,27 @@
 
 "Check the health of Firefox bookmarks."
 
-import sqlite3
+import argparse
+import itertools as itert
 import multiprocessing as mp
+import socket
+import sqlite3
+import sys
 import urllib.request as rq
 from urllib.error import URLError
-import socket
-import itertools as itert
 
 query = "select url from moz_places inner join moz_bookmarks on moz_places.id = moz_bookmarks.fk"
 useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
-connection = sqlite3.connect('places.sqlite')
-cursor = connection.cursor()
-results = map(lambda x: x[0],
-              filter(lambda x: x[0].startswith("http"), cursor.execute(query)))
-
 report_success = False
 report_redirects = False
 timeout = 10
+database = "places.sqlite"
+
+connection = sqlite3.connect(database)
+cursor = connection.cursor()
+results = map(lambda x: x[0],
+              filter(lambda x: x[0].startswith("http"), cursor.execute(query)))
 
 success = mp.Queue()
 failure = mp.Queue()
@@ -75,5 +78,28 @@ def run(nprocs=10, nresults=None):
     print("Checked {} urls: {} healty, {} dead (%{} linkrot)".format(
         total, nsuccess, nfailure, nfailure / total * 100))
 
+def cli(args):
+    p = argparse.ArgumentParser(
+        description="Check the health of Firefox bookmarks.")
+    p.add_argument("db", metavar="DB", type=str,
+        help="the path to places.sqlite file (preferably a copy,"
+             " not the actual one Firefox uses)")
+    p.add_argument("-a", type=str, metavar="USER-AGENT",
+        help="the user agent string to usewhen making requests,"
+             " by default set to resemble a web browser")
+    p.add_argument("-v", action="count", help="be verbose")
+    p.add_argument("-t", metavar="SECONDS", type=int,
+        help="connection timeout")
+
+    args = p.parse_args(args)
+
+    global report_success, report_redirects, timeout, database
+    if args.v:
+        if args.v >= 1: report_success = True
+        if args.v >= 2: report_redirects = True
+    if args.t: timeout = args.t
+    database = args.db
+
 if __name__ == "__main__":
+    cli(sys.argv[1:])
     run()
